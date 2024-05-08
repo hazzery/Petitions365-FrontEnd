0c59799d9083175d93649ec9505acaaec5d80cdf
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-08 13:25:17 +1200

    updated eslint config to enforce semicolons and to enforce correct comma delimited interfaces/types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,32 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
     ],
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true},
+        ],
+        '@typescript-eslint/member-delimiter-style': [
+            'warn',
+            {
+                'multiline': {
+                    'delimiter': 'comma',
+                    'requireLast': false
+                },
+                'singleline': {
+                    'delimiter': 'comma',
+                    'requireLast': false
+                }
+            }
+        ],
+        'semi': ['error', 'always']
+    },
 }
