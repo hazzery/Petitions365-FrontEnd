d06897333e84bbd1fb49b7f4e990eb3cfe50b767
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-10 12:56:11 +1200

    added searchbar to petitions page

--- src/components/Petitions.tsx
+++ src/components/Petitions.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import {AxiosResponse} from "axios";
+
 
 import {Category, PetitionOverview, PetitionsList} from "../model/responseBodies.ts";
 import {getAllCategories, getAllPetitions} from "../model/api.ts";
 import PetitionCard from "./PetitionCard.tsx";
-import {AxiosResponse} from "axios";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 
 export default function Petitions() {
     const [petitions, setPetitions] = React.useState<Array<PetitionOverview>>([]);
     const [categoryMap, setCategoryMap] = React.useState<Map<number, string>>(new Map<number, string>());
+    const [searchQuery, setSearchQuery] = React.useState<string>("");
 
     React.useEffect(() => {
         getAllPetitions()
@@ -34,15 +40,37 @@ export default function Petitions() {
             });
     }, []);
 
+    function submitSearchQuery(event: React.FormEvent<HTMLDivElement>) {
+        console.log(event);
+    }
+
     function petitionCards() {
         return petitions.map(
-            (petition: PetitionOverview) => <PetitionCard petitionOverview={petition} categoryMap={categoryMap}/>
+            (petition: PetitionOverview) => <PetitionCard
+                key={petition.petitionId}
+                petitionOverview={petition}
+                categoryMap={categoryMap}
+            />
         );
     }
 
     return (
         <div>
             <h1>Petitions</h1>
+            <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <TextField
+                    id="search"
+                    label="Search"
+                    variant="outlined"
+                    sx={{width: '50%'}}
+                    value={searchQuery}
+                    onChange={(event) => setSearchQuery(event.target.value)}
+                    onSubmit={submitSearchQuery}
+                />
+                <Button variant="contained" color="primary" sx={{marginLeft: 1}}>
+                    <SearchIcon sx={{fontSize: 30}}/>
+                </Button>
+            </Box>
             <div className="petitions-grid">
                 {petitionCards()}
             </div>
