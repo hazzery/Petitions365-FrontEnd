e99fa3cb860f17300673b465b135d6114c7286a0
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-13 16:35:55 +1200

    Searching for petitions now works!

--- src/components/Petitions.tsx
+++ src/components/Petitions.tsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React, {useCallback} from "react";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import {AxiosResponse} from "axios";
 
 
 import {Category, PetitionOverview, PetitionsList} from "../model/responseBodies.ts";
-import {getAllCategories, getAllPetitions} from "../model/api.ts";
+import {getAllCategories, getAllPetitions, getFilteredPetitions} from "../model/api.ts";
 import PetitionCard from "./PetitionCard.tsx";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 
@@ -15,9 +14,10 @@ export default function Petitions() {
     const [petitions, setPetitions] = React.useState<Array<PetitionOverview>>([]);
     const [categoryMap, setCategoryMap] = React.useState<Map<number, string>>(new Map<number, string>());
     const [searchQuery, setSearchQuery] = React.useState<string>("");
+    const [searchInput, setSearchInput] = React.useState<string>("");
 
-    React.useEffect(() => {
-        getAllPetitions()
+    const submitSearchQuery = useCallback(() => {
+        (searchQuery.length == 0 ? getAllPetitions() : getFilteredPetitions({q: searchQuery}))
             .then((response: AxiosResponse<PetitionsList>) => {
                 setPetitions(response.data.petitions);
             })
@@ -25,7 +25,9 @@ export default function Petitions() {
                 console.log(error.response.status);
                 console.log(error.response.statusText);
             });
+    }, [searchQuery]);
 
+    React.useEffect(() => {
         getAllCategories()
             .then((response: AxiosResponse<Array<Category>>) => {
                 const map = new Map<number, string>();
@@ -38,11 +40,8 @@ export default function Petitions() {
                 console.log(error.response.status);
                 console.log(error.response.statusText);
             });
-    }, []);
-
-    function submitSearchQuery(event: React.FormEvent<HTMLDivElement>) {
-        console.log(event);
-    }
+        submitSearchQuery();
+    }, [submitSearchQuery]);
 
     function petitionCards() {
         return petitions.map(
@@ -57,20 +56,23 @@ export default function Petitions() {
     return (
         <div>
             <h1>Petitions</h1>
-            <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <form
+                style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}
+                onSubmit={() => setSearchQuery(searchInput)}
+                method="dialog"
+            >
                 <TextField
                     id="search"
                     label="Search"
                     variant="outlined"
                     sx={{width: '50%'}}
-                    value={searchQuery}
-                    onChange={(event) => setSearchQuery(event.target.value)}
-                    onSubmit={submitSearchQuery}
+                    value={searchInput}
+                    onChange={(event) => setSearchInput(event.target.value)}
                 />
-                <Button variant="contained" color="primary" sx={{marginLeft: 1}}>
+                <Button variant="contained" color="primary" type="submit" sx={{marginLeft: 1}}>
                     <SearchIcon sx={{fontSize: 30}}/>
                 </Button>
-            </Box>
+            </form>
             <div className="petitions-grid">
                 {petitionCards()}
             </div>
--- src/model/api.ts
+++ src/model/api.ts
@@ -37,6 +37,82 @@ export function getAllPetitions(): Promise<AxiosResponse<PetitionsList>> {
     return Axios.get(rootUrl + "/petitions");
 }
 
+type SortOrder = "ALPHABETICAL_ASC" | "ALPHABETICAL_DESC" | "COST_ASC" | "COST_DESC" | "CREATED_ASC" | "CREATED_DESC";
+
+interface GetFilteredPetitionsParams {
+    startIndex?: number,
+    count?: number,
+    q?: string,
+    categoryIds?: number[],
+    supportingCost?: number,
+    ownerId?: number,
+    supporterId?: number,
+    sortBy?: SortOrder
+}
+
+/**
+ * Fetches all petitions that match the given filters.
+ * @param startIndex The index of the first petition to fetch.
+ * @param count The number of petitions to fetch.
+ * @param q The search query to filter petitions by.
+ * Results will only include petitions with titles or descriptions that contain this string.
+ * @param categoryIds The IDs of categories in which petitions must be part of.
+ * Results will only include petitions that are in at least one of these categories.
+ * @param supportingCost The highest minimum cost to support a petition.
+ * Results will only include petitions with a minimum cost to support that is less than or equal to this value.
+ * @param ownerId The ID of a user.
+ * Results will only include petitions that are owned by this user.
+ * @param supporterId The ID of a user.
+ * Results will only include petitions that are supported by this user.
+ * @param sortBy Sort the petitions by the given property, according to the following rules:
+ * ALPHABETICAL_ASC: alphabetically by title, A-Z
+ * ALPHABETICAL_DESC: alphabetically by title, Z-A
+ * COST_ASC: by cost of cheapest support tier ascending
+ * COST_DESC: by cost of cheapest support tier descending
+ * CREATED_ASC: chronologically in order of creation date oldest-newest
+ * CREATED_DESC: chronologically in order of creation date newest-oldest
+ */
+export function getFilteredPetitions(
+    {
+        startIndex,
+        count,
+        q,
+        categoryIds,
+        supportingCost,
+        ownerId,
+        supporterId,
+        sortBy
+    }: GetFilteredPetitionsParams
+): Promise<AxiosResponse<PetitionsList>> {
+    const params = new URLSearchParams();
+
+    if (startIndex !== undefined) {
+        params.append("startIndex", startIndex.toString());
+    }
+    if (count !== undefined) {
+        params.append("count", count.toString());
+    }
+    if (q !== undefined) {
+        params.append("q", q);
+    }
+    if (categoryIds !== undefined && categoryIds.length > 0) {
+        params.append("categoryIds", categoryIds.join(","));
+    }
+    if (supportingCost !== undefined) {
+        params.append("supportingCost", supportingCost.toString());
+    }
+    if (ownerId !== undefined) {
+        params.append("ownerId", ownerId.toString());
+    }
+    if (supporterId !== undefined) {
+        params.append("supporterId", supporterId.toString());
+    }
+    if (sortBy !== undefined) {
+        params.append("sortBy", sortBy);
+    }
+    return Axios.get(rootUrl + "/petitions?" + params.toString());
+}
+
 /**
  * Fetches the details of a petition.
  *
