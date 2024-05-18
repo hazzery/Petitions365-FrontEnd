659d851b68f74e60520fb1c7f1ae762af9f7af75
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-19 11:40:44 +1200

    Removed "Forbidden" from duplicate email error message

--- src/model/util.ts
+++ src/model/util.ts
@@ -8,7 +8,7 @@ export function formatDate(timestamp: string) {
 }
 
 export function formatServerResponse(message: string) {
-    const str = message.replace('Bad Request: data/', '');
+    const str = message.replace('Bad Request: data/', '').replace('Forbidden: ', '');
     const split =  str.split(' ');
     // return the first word of str converted from `camelCase` to `Sentence case` prepended to the rest of the message.
     return split[0][0].toUpperCase() + split[0].slice(1).replace(/([A-Z])/g,' $1') + ' ' + split.slice(1).join(' ');
