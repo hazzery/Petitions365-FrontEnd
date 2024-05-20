6a2eabfc628ec1320e6c83c6828f4adb5f63b1da
Harry Parkes <harrydparkes@proton.me>
2024-05-20 20:12:06 +1200

    added petitions button to landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -8,6 +8,7 @@ export default function Landing() {
             <h1>Welcome to SENG365 Petition Site</h1>
             <Button variant="contained" color="primary" href="/login">Login</Button>
             <Button variant="contained" color="primary" href="/register">Register</Button>
+            <Button variant="contained" color="primary" href="/petitions">Petitions</Button>
         </div>
     );
 }
