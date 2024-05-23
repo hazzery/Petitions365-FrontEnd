083b18b3f0f06543a27e61773544ca62fa30ffa9
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-24 09:40:16 +1200

    Made landing page more consistent with the rest of the application

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,32 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
 import {Button} from "@mui/material";
-// type LandingProps = Record<string, never>;
 
 
-export default function Landing() {
+const defaultTheme = createTheme();
+
+export default function Landing(): React.ReactElement {
     return (
-        <div>
-            <h1>Welcome to SENG365 Petition Site</h1>
-            <Button variant="contained" color="primary" href="/login">Login</Button>
-            <Button variant="contained" color="primary" href="/register">Register</Button>
-            <Button variant="contained" color="primary" href="/petitions">Petitions</Button>
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <Typography variant="h3">Welcome to Petitions365</Typography>
+                <Box sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-evenly',
+                    alignItems: 'center',
+                }}>
+                    <Button variant="contained" color="primary" href="/login">Login</Button>
+                    <Button variant="contained" color="primary" href="/register">Register</Button>
+                    <Button variant="contained" color="primary" href="/petitions">Petitions</Button>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
