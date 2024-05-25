f795385b24f0e3cb0121ee9dd1ef329cbeb2c929
Harry Parkes <harrydparkes@proton.me>
2024-05-26 09:43:58 +1200

    CostInput now uses number validation hook

--- src/components/CostInput.tsx
+++ src/components/CostInput.tsx
@@ -2,6 +2,8 @@ import React, {ChangeEvent} from "react";
 import TextField from "@mui/material/TextField";
 import {InputAdornment} from "@mui/material";
 
+import useNumberValidation from "../hooks/useNumberValidation.ts";
+
 
 interface CostInputProps<Required extends boolean> {
     required?: Required,
@@ -9,20 +11,33 @@ interface CostInputProps<Required extends boolean> {
     label: string,
     defaultValue?: number | "",
     onChange: Required extends true ? (value: number) => void : (value: number | "") => void,
-    disabled?: boolean
+    disabled?: boolean,
+    formSubmitted?: boolean
 }
 
 export default function CostInput<Required extends boolean>(
-    {required, fullWidth, label, defaultValue, onChange, disabled}: CostInputProps<Required>
+    {
+        required = false as Required,
+        fullWidth = false,
+        label,
+        defaultValue = "" as const,
+        onChange,
+        disabled = false,
+        formSubmitted = false
+    }: CostInputProps<Required>
 ): React.ReactElement {
-    const [value, setValue] = React.useState<string>(defaultValue ? defaultValue.toString() : "");
+    const [value, setValue] = useNumberValidation({
+        required,
+        minValue: 0,
+        maxValue: 999999999
+    }, defaultValue.toString());
     const [textFieldIsFocused, setTextFieldIsFocused] = React.useState<boolean>(false);
     const [error, setError] = React.useState<boolean>(false);
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement>): void {
         if (event.target.value === "") {
             setValue("");
-            setError(required ?? false);
+            setError(formSubmitted && (required ?? false));
             if (!required) {
                 (onChange as (value: number | "") => void)("");
             }
@@ -39,16 +54,17 @@ export default function CostInput<Required extends boolean>(
     return (
         <TextField
             label={label}
-            value={value}
+            value={value.value}
             fullWidth={fullWidth}
             required={required}
             onChange={handleInputChange}
-            error={error}
+            error={(formSubmitted && Boolean(value.error)) || error}
+            helperText={formSubmitted && value.error}
             disabled={disabled}
             InputProps={{startAdornment: <InputAdornment position="start">$</InputAdornment>}}
             InputLabelProps={{
-                shrink: textFieldIsFocused || value !== "",
-                style: {paddingLeft: !textFieldIsFocused && value === "" ? "15px" : "0px"}
+                shrink: textFieldIsFocused || value.value !== "",
+                style: {paddingLeft: !textFieldIsFocused && value.value === "" ? "15px" : "0px"}
             }}
             onFocus={() => setTextFieldIsFocused(true)}
             onBlur={() => setTextFieldIsFocused(false)}
