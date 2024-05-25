2a8c289183570dd55c2069d0c0334e4ab3a7fbbe
Harrison Parkes <hpa101@uclive.ac.nz>
2024-05-25 13:25:12 +1200

    Made NotFound page more consistent with all other pages

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,22 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import NavBar from "../components/NavBar.tsx";
+
+
+const defaultTheme = createTheme();
+
 export default function NotFound() {
     return (
-        <div>
-            <h1>Oopsy Daisy, that page can't be found!</h1>
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <NavBar/>
+            <Container component="main" maxWidth="xl">
+                <CssBaseline/>
+                <Typography variant="h3" sx={{marginTop: "30px"}}>
+                    Oopsy daisy, that page can't be found!
+                </Typography>
+            </Container>
+        </ThemeProvider>
     );
 }
